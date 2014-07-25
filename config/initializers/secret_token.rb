4d704c3ff5af3350b1cf38cd1f4a762805c4a364
Jered McCullough <mathias.armstrong@gmail.com>
2014-07-25 13:21:42 -0700

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuizzyPersonal::Application.config.secret_key_base = '6be9b8ad369f65b6b65ef1a9f2bfa14dc7c4dc9aaecdba4615349d0d6dab6aa01de53f8b2c5b860c966fe32d77e704fae10a2e0472e0ef49ea5fbb9be5c49dbe'
